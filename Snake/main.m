b4f8d5ba1784131e03aac846e6537b41368431e1
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2016-01-19 12:11:03 +0300

    Initial Commit

---
+++ Snake/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snake
+//
+//  Created by Mikhail Kulichkov on 19/01/16.
+//  Copyright © 2016 kulichkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
